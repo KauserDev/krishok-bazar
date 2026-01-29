b5b992d67b1e60e2ccf1946534a0a8f64f8c8aac
Kauser Hossen <kauserhossendev@gmail.com>
2026-01-29 13:42:52 +0600

    Updated firebase config to use env variables

--- src/firebase.js
+++ src/firebase.js
@@ -4,19 +4,17 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCn0m2NyhqtwbeG6umrjuDPmbcEGeFwwfo",
-  authDomain: "krishok-bazar.firebaseapp.com",
-  projectId: "krishok-bazar",
-  storageBucket: "krishok-bazar.firebasestorage.app",
-  messagingSenderId: "375319143106",
-  appId: "1:375319143106:web:f9c09e8c32b42cf1aa115b",
-  measurementId: "G-WGV2DYH74D"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// এগুলি অবশ্যই export করতে হবে যাতে AuthContext এ কাজ করে
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
